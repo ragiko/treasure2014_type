ac9def1c9a174b57e0da3e426974110834c54d73
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-09-06 16:36:12 +0900

    typingして完了すると時間とまる

--- api.js
+++ api.js
@@ -1,3 +1,5 @@
+// textをspanが込み
+
 (function(w, $, Vue) {
     'use strict';
 
@@ -5,27 +7,48 @@
         el: '#demo',
         data: {
             message: 0,
+            typeTextArray: "",
             startTime: null,
+            timer: null,
         },
         ready: function () {
             this.start();
+            this.makeTypeText();
             this.timeCount();
         },
         methods: {
             start: function () {
+                // dataを取得
                 var now = new Date();
                 this.startTime = now.getTime();
+                
+                // inputを空白にして空白
+                $(".type-input").val("").focus();
             },
             timeCount: function () {
-                setInterval(function () {
-                    var now = new Date();
-                    
-                    w.app.$data.message = now.getTime() - w.app.$data.startTime;
-                }, 1);
-            },
-            test: function () {
-                // $(".type-input").text();
+                var that = this;
 
+                this.timer = setInterval(function () {
+                    var now = new Date();
+
+                    that.message = (now.getTime() - that.startTime)/1000;
+                }, 1);
+
+            },
+            // タイピング用のDOM作成
+            makeTypeText: function () {
+                this.typeTextArray = "treasure2014".split("");
+            }, 
+            onKeyType: function () {
+                var $t = $(".type-char:not(.completed)").first(
+
+                if ($t.text() === $(".type-input").val().slice(-1)) {
+                    $t.addClass("completed");
+                } 
+
+                if ($(".type-char:not(.completed)").length == 0 ) {
+                    clearInterval(this.timer);
+                }
             }
         },
     });
